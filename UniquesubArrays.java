35c47d3703d2ba3ae990e8c567be1691d5720f3a
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-12-02 23:27:54 -0500

    test: added test to count the unique subarrays that sum to a targert value

---
+++ UniquesubArrays.java
@@ -0,0 +1,8 @@
+public class UniquesubArrays {
+    
+    public static void main(String[] args) {
+        int[] arr1 = {4, 1, 2, 7, 3};
+        int target1 = 6;
+        System.out.println("Test Case 1 - Expected: 2, Result: " + countUniquesubArrays(arr1, target1));
+    }   
+}
