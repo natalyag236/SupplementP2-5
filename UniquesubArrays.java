b10952cf27be023f7910c2a0523bef42deda85c0
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-12-02 23:32:14 -0500

    feat:Implemented logic to count unique subarrays summing to a targert value

--- UniquesubArrays.java
+++ UniquesubArrays.java
@@ -1,4 +1,27 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class UniquesubArrays {
+    public static int  countUniquesubArrays(int[] arr, int target) {
+         Set<String> uniqueSubarrays = new HashSet<>();
+        int n = arr.length;
+
+        for (int start = 0; start < n; start++) {
+            int sum = 0;
+            StringBuilder subarray = new StringBuilder();
+
+            for (int end = start; end < n; end++) {
+                sum += arr[end];
+                subarray.append(arr[end]).append(",");
+                
+                if (sum == target) {
+                    uniqueSubarrays.add(subarray.toString());
+                }
+            }
+        }
+        
+        return uniqueSubarrays.size();
+    }
     
     public static void main(String[] args) {
         int[] arr1 = {4, 1, 2, 7, 3};
