cd08085ba51131f35f469690e1c23eab7e06016b
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-12-02 23:46:06 -0500

    feat: added code to find the longest palindromic substring

--- longestpalindromic.java
+++ longestpalindromic.java
@@ -1,7 +1,39 @@
 public class longestpalindromic {
+        public static boolean checkPal(String s, int low, int high) {
+            while (low < high) {
+                if (s.charAt(low) != s.charAt(high))
+                    return false;
+                low++;
+                high--;
+            }
+            return true;
+        }
+    
+    
+        public static String longestPalindromic(String s) {
+            int n = s.length();
+            
+            int maxLen = 1, start = 0;
+    
+            for (int i = 0; i < n; i++) {
+                for (int j = i; j < n; j++) {
+                  
+                 
+                    if (checkPal(s, i, j) && (j - i + 1) > maxLen) {
+                        start = i;
+                        maxLen = j - i + 1;
+                    }
+                }
+            }
+    
+            return s.substring(start, start + maxLen);
+        }
+    
+       
+    
     public static void main(String[] args) {
         String s1 = "christmas";
-        System.out.println("Test Case 1 - Expected: christmas, Result: " + longestPalindrome(s1));
+        System.out.println("Test Case 1 - Expected: christmas, Result: " + longestPalindromic(s1));
 
        
     }
