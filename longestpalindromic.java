5afa14853fb252c5ad533c447ddfce4a5bb5bc49
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-12-02 23:39:51 -0500

    test: implemented the longest palindrome substring algorithm

---
+++ longestpalindromic.java
@@ -0,0 +1,9 @@
+public class longestpalindromic {
+    public static void main(String[] args) {
+        String s1 = "christmas";
+        System.out.println("Test Case 1 - Expected: christmas, Result: " + longestPalindrome(s1));
+
+       
+    }
+}
+
