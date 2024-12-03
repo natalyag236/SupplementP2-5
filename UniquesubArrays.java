1864a52e5e7b5272df9affecca82c7ebffda94ae
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-12-02 23:33:40 -0500

    doc: Added documentation to the countUniquesubArrays method.

--- UniquesubArrays.java
+++ UniquesubArrays.java
@@ -2,6 +2,14 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class UniquesubArrays {
+    /**
+     * This method counts the number of unique subarrays in the given array that sum up to the target value.
+     * It uses a HashSet to store subarrays as strings, ensuring only unique subarrays are counted.
+     *
+     * @param arr The input array of integers
+     * @param target The target sum for the subarrays
+     * @return The number of unique subarrays that sum to the target
+     */
     public static int  countUniquesubArrays(int[] arr, int target) {
          Set<String> uniqueSubarrays = new HashSet<>();
         int n = arr.length;
