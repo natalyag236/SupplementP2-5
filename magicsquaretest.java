504648735c39defefea98ff911fff3229bc42e1e
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-12-02 23:07:07 -0500

    test: add test case for valid 3x3 magic square

---
+++ magicsquaretest.java
@@ -0,0 +1,11 @@
+public class magicsquaretest {
+
+    public static void main(String[] args) {
+        int[][] matrix1 = {
+            {2, 7, 6},
+            {9, 5, 1},
+            {4, 3, 8}
+        };
+        System.out.println("Test Case 1 - Expected: true, Result: " + MagicSquare.isMagicSquare(matrix1));
+    }
+}
